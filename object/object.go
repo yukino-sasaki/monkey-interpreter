e657bdff3ea92052ff1f76629cdbce736d191b1d
yukino-sasaki <yukino@oisu.jp>
2023-01-20 16:24:43 +0900

    feat: return integer

--- object/object.go
+++ object/object.go
@@ -1,8 +1,55 @@
 package object
 
+import (
+	"fmt"
+)
+
 type ObjectType string
 
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ = "NULL"
+)
+
+// 全ての値がOBject Interfaceを満たす構造体にラップされるようにする
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+type Integer struct {
+	Value int64
+}
+
+type Boolean struct {
+	Value bool
+}
+
+type Null struct {}
+
+// 整数リテラルに出会うたびにast.IntegerLiteralに変換、ASTを評価数r際にobject.Integerへ変換
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+func (n *Null) Inspect() string {
+	return "null"
+}
+
+
