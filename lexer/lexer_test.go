aa60e8eb7f8cfa6961af6868903f94b9fff4e850
yukino-sasaki <yukino@oisu.jp>
2023-01-31 16:44:46 +0900

    add lexer of hash

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -29,6 +29,7 @@ func TestNextToken(t *testing.T) {
 	"foobar"
 	"foo bar"
 	[1,2];
+	{"foo": "bar"}
 `
 	
 
@@ -117,12 +118,12 @@ func TestNextToken(t *testing.T) {
 		{token.INT, "2"},
 		{token.RBRACKET, "]"},
 		{token.SEMICOLON, ";"},
-		// {token.LBRACE, "{"},
-		// {token.STRING, "foo"},
-		// {token.COLON, ":"},
-		// {token.STRING, "bar"},
-		// {token.RBRACE, "}"},
-		// {token.EOF, ""},
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
+		{token.EOF, ""},
 	}
 
 	l := New(input)
